d2f3c9cfed02aefb0abf37fda4534ec6f0b4e9f3
sachinio99 <sachinshah0099@gmail.com>
2024-06-22 17:22:22 -0400

    cli.py is going to have better agent code

--- cli.py
+++ cli.py
@@ -1,20 +1,49 @@
 from pathlib import Path 
 import argparse 
+from openai import OpenAI
+import os
 
 
-
+model = 'gpt-4'
+client = OpenAI(
+    api_key= os.getenv("OPENAI_API_KEY")
+    
+)
 def main():
     parser = argparse.ArgumentParser(description="CLI for getting params for API Call")
     parser.add_argument("user_location")
     args = parser.parse_args()
     print(args.user_location)
 
+def chat_completion_request(messages):
+    try:
+        response = client.chat.completions.create(
+            model=model,
+            messages=messages,
+        )
+        return response
+    except Exception as e:
+        print("Unable to generate ChatCompletion response")
+        print(f"Exception: {e.status_code}")
+        return e
+
+GPT_MODEL = "gpt-4"
+# Set your OpenAI API key from the environment variable
+client = OpenAI(
+    api_key = os.getenv("OPENAI_API_KEY")
+)
+
+messages = []
+messages.append({"role": "system", "content": "Don't make assumptions about what song to search for or what functions to use. Make sure to use the songs in the list returned based on the user's location"})
+messages.append({"role": "system", "content": "If the location is missing, make sure to prompt the user for their current location and then use that in your search. Thanks! "})
+messages.append({"role": "user", "content": "Make me a playlist of underground songs from several different genres based on my location. "})
+messages.append({"role": "assistant", "content": "Sure, I'll get right on that. Please provide your location in the following format: '{city},{state}'"})
 
 
-
-
-
-
+chat_response = chat_completion_request(
+    messages
+)
+print(chat_response)
 
 if __name__ == "__main__":
     main()
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Add the ngrok authtoken
- ngrok authtoken 2hQxB1ZLybpRWy9xcLaHVnXZtw0_5r1v1e2FWMZsXmCWPYSdL
+ngrok authtoken 2hQxB1ZLybpRWy9xcLaHVnXZtw0_5r1v1e2FWMZsXmCWPYSdL
 
 # Start ngrok in the background
 ngrok http 8000 > /dev/null &
@@ -9,7 +9,7 @@ ngrok http 8000 > /dev/null &
 #ngrok http http://localhost:8000
 
 # Give ngrok some time to start and retrieve the URL
- sleep 5
+sleep 5
 
 # Print the ngrok public URL
 # curl --silent --max-time 10 --connect-timeout 5 --retry 2 http://127.0.0.1:4040/api/tunnels | jq -r '.tunnels[0].public_url'
